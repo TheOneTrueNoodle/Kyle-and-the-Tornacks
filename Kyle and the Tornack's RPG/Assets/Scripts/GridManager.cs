28af26bf321049b96dfba36d55605d6dcaa9cc3c
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-06-24 16:21:17 +0100

    Setup for accessing the selected tile

--- Kyle and the Tornack's RPG/Assets/Scripts/GridManager.cs
+++ Kyle and the Tornack's RPG/Assets/Scripts/GridManager.cs
@@ -10,6 +10,9 @@ public class GridManager : MonoBehaviour
 
     [SerializeField] private Transform cam;
 
+
+    private Dictionary<Vector2, Tile> Tiles;
+
     private void Start()
     {
         GenerateGrid();
@@ -17,6 +20,7 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        Tiles = new Dictionary<Vector2, Tile>();
         for(int x = 0; x < width; x++)
         {
             for(int y = 0; y < height; y++)
@@ -24,9 +28,18 @@ public class GridManager : MonoBehaviour
                 var spawnedTile = Instantiate(tilePrefab, new Vector3(gameObject.transform.position.x + x, gameObject.transform.position.y + y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
                 spawnedTile.transform.parent = gameObject.transform;
+
+                Tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
+    }
 
-        
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if(Tiles.TryGetValue(pos, out var tile))
+        {
+            return tile;
+        }
+        return null;
     }
 }
