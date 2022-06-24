cf19a668eb178ca209b881409c5c3e28a5644c40
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-06-24 17:47:48 +0100

    Bunch of tile updating and camera work WIP

--- Kyle and the Tornack's RPG/Assets/Scripts/GridManager.cs
+++ Kyle and the Tornack's RPG/Assets/Scripts/GridManager.cs
@@ -7,30 +7,24 @@ public class GridManager : MonoBehaviour
     [SerializeField] private int width, height;
 
     [SerializeField] private Tile tilePrefab;
+    private Tile[] tileGrid;
 
-    [SerializeField] private Transform cam;
-
-
-    private Dictionary<Vector2, Tile> Tiles;
+    public Dictionary<Vector2, Tile> Tiles;
 
     private void Start()
     {
-        GenerateGrid();
+        FindGrid();
+        //GenerateGrid();
     }
 
-    void GenerateGrid()
+    void FindGrid()
     {
+        tileGrid = GetComponentsInChildren<Tile>();
         Tiles = new Dictionary<Vector2, Tile>();
-        for(int x = 0; x < width; x++)
-        {
-            for(int y = 0; y < height; y++)
-            {
-                var spawnedTile = Instantiate(tilePrefab, new Vector3(gameObject.transform.position.x + x, gameObject.transform.position.y + y), Quaternion.identity);
-                spawnedTile.name = $"Tile {x} {y}";
-                spawnedTile.transform.parent = gameObject.transform;
 
-                Tiles[new Vector2(x, y)] = spawnedTile;
-            }
+        foreach (Tile tile in tileGrid)
+        {
+            Tiles[new Vector2(tile.transform.position.x, tile.transform.position.y)] = tile;
         }
     }
 
@@ -41,5 +35,18 @@ public class GridManager : MonoBehaviour
             return tile;
         }
         return null;
+    }
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(gameObject.transform.position.x + x, gameObject.transform.position.y + y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+                spawnedTile.transform.parent = gameObject.transform;
+            }
+        }
     }
 }
